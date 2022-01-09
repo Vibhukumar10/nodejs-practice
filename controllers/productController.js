91790d8d1ee8073ba021100bbd4e2ef4882ab310
Vibhu <vibhuedits@gmail.com>
2022-01-10 00:14:44 +0530

    Get products by Id

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,5 +1,7 @@
 const Product = require('../models/productModel')
 
+// @desc  Gets All Products
+// @route GET /api/products
 const getProducts = async (req, res) => {
     try {
         const products = await Product.findAll()
@@ -10,6 +12,24 @@ const getProducts = async (req, res) => {
     }
 }
 
+// @desc  Get Product by ID
+// @route GET /api/product/:id
+const getProduct = async (req, res, id) => {
+    try {
+        const product = await Product.findById(id)
+        if (!product) {
+            res.writeHead(404, { 'Content-Type': 'application/json' })
+            res.end(JSON.stringify({ message: 'Product Not Found!' }))
+        } else {
+            res.writeHead(200, { 'Content-Type': 'application/json' })
+            res.end(JSON.stringify(product))
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
 module.exports = {
     getProducts,
+    getProduct,
 }
--- models/productModel.js
+++ models/productModel.js
@@ -6,6 +6,14 @@ const findAll = () => {
     })
 }
 
+const findById = (id) => {
+    return new Promise((resolve, reject) => {
+        const product = products.find((product) => product.id === id)
+        resolve(product)
+    })
+}
+
 module.exports = {
     findAll,
+    findById,
 }
